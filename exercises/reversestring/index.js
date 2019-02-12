6b22142981b0888b8ecf5785cab8fcab3499f7af
Scott Horn <shorn813@outlook.com>
2019-02-12 15:33:06 -0500

    reverse string complete

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    //  SOLUTION 1
+    return str.split('').reverse().join('');
+
+
+    // SOLUTION 2
+    // let reversed = '';
+
+    // for (let char of str) {
+    //     reversed = char + reversed;
+    // }
+
+    // return reversed;
+
+
+    // SOLUTION 3
+    // return str.split('').reduce((rev, char) => char + rev, '');
+
+}
 
 module.exports = reverse;
